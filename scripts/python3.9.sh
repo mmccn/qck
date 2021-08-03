4aaffe8de31fa1670275ddd4d95f862d463aa9bc
OofChair <oofchairr@gmail.com>
2021-08-03 15:12:00 -0400

    Add python3.9 scripts

---
+++ scripts/python3.9.sh
@@ -0,0 +1,26 @@
+# Since this script is already written and maintained, we are just calling a curl command to execute this script.
+
+if [ which curl &> /dev/null ]
+then	
+	OS_VERSION = grep '^ID_LIKE' /etc/os-release
+	if [ OS_VERSION == *"ubuntu"* ] || [ OS_VERSION == *"debian"* ]
+	then
+		echo "Detected debian-based install, installing wget"
+		sudo apt install wget make tar
+	else
+		echo "Please install wget, make, and tar then re-run this script!"
+	fi
+fi
+
+echo "Installing Node (LTS Release)"
+echo "NOTE: This requires root privileges"
+if [[ ( -n "$DEBUG" ) ||( "$DEBUG" != true) ]]
+then
+	curl -P ~/.qck/python3.9 https://www.python.org/ftp/python/3.9.6/Python-3.9.6.tgz
+	cd ~/.qck/python3.9
+	./configure
+	make
+	sudo make install
+else
+	echo "Debug mode activated, not calling install script"
+fi
